e8aa0cbc05f4c170e0ab14ff3edf06bd8cd833e8
Christian Ndu <christiannduh@gmail.com>
2023-02-15 12:32:20 +0100

    feat: profile page works

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -6,16 +6,19 @@ import { useEffect } from "react";
 const UserProfile: React.FC = () => {
     const { query } = useRouter();
     const { username } = query;
+    const [loading, setLoading] = useState(false);
 
     const [user, setUser] = useState<any>({});
 
     const getUserFromDB = async () => {
         try {
+            setLoading(true);
             const user = await axios.post("/api/connect/spotify", {
                 action: "get",
                 username: username,
             });
             setUser(user.data.data);
+            setLoading(false);
         } catch (error) {
             console.error(error);
         }
@@ -27,8 +30,23 @@ const UserProfile: React.FC = () => {
 
     return (
         <>
-            <p className="text-white">Hi {username}</p>
-            <p className="text-white">{user.username} wow!!</p>
+            <div className="text-white text-center my-20">
+                {loading ? (
+                    <div className="bg-indigo-500 text-center">
+                        <svg
+                            className="animate-spin h-5 w-5 mr-3"
+                            viewBox="0 0 24 24"
+                        ></svg>
+                        Fetching your details...
+                    </div>
+                ) : (
+                    <div className="">
+                        <p className=" text-6xl text-secondary">
+                            Welcome {user.username}
+                        </p>
+                    </div>
+                )}
+            </div>
         </>
     );
 };
